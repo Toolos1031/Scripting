af5d98ea0270de88b04b7fdef5ac2987200fc1e8
Kate <bartosz.skrabanek@bzbuas.com>
2025-06-24 13:00:35 +0200

    new files

---
+++ Las_clipping_godlo_mapy.py
@@ -0,0 +1,176 @@
+import geopandas as gpd
+from shapely.geometry import box, Point
+import os
+from tqdm import tqdm
+import laspy
+import numpy as np
+from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
+import subprocess
+from collections import defaultdict
+
+root_folder = r"D:\___WodyPolskie\Ostrzeszow\przetwarzanie\Gotowe\__Przycinanie_godlo"
+
+scan_folder = os.path.join(root_folder, "las")
+out_folder = os.path.join(root_folder, "out")
+joined_folder = os.path.join(root_folder, "joined")
+shapefile = os.path.join(root_folder, "PL1992_5000.shp")
+
+folder_list = [out_folder, joined_folder]
+las_files = [os.path.join(scan_folder, f) for f in os.listdir(scan_folder) if f.endswith(".las")]
+
+def check_root():
+    if os.path.isdir(root_folder):
+        for folder in folder_list:
+            name = folder.split("\\")[-1]
+            if not os.path.isdir(folder):
+                os.mkdir(folder)
+                print(f"Created folder for {name}")
+    else:
+        input("Root folder does not exist, PRESS ENTER TO EXIT")
+        raise SystemExit(0)
+    
+    if not os.path.isfile(shapefile):
+        input("Shapefile not found, PRESS ENTER TO EXIT")
+        raise SystemExit(0)
+
+    if not las_files:
+        input("No .las files in directory, PRESS ENTER TO EXIT")
+        raise SystemExit(0)
+    
+def bbox(scan): #bounding box of the scan
+    scan_extent = {
+        "xmin" : scan.header.mins[0],
+        "xmax" : scan.header.maxs[0],
+        "ymin" : scan.header.mins[1],
+        "ymax" : scan.header.maxs[1]
+    }
+
+    scan_bbox = box(
+        scan_extent["xmin"],
+        scan_extent["ymin"],
+        scan_extent["xmax"],
+        scan_extent["ymax"]
+    )
+    
+    return scan_bbox
+
+def check_points(args): #check if points are inside of a polygon (used for multiprocessing)
+    chunk, polygon = args
+    return [polygon.contains(Point(x, y)) for x, y in chunk]
+
+def process_scans(scan_file): #main function
+    las = laspy.read(scan_file)
+    shape = gpd.read_file(shapefile)
+
+    #Take las and make it usable for later
+    points = np.vstack((las.x, las.y)).T 
+
+    #Get only polygons that intersect the point cloud
+    intersecting_polygons = shape[shape.geometry.intersects(bbox(las))]
+
+    for rows, cols in intersecting_polygons.iterrows():
+        print(cols['godlo'])
+        print(scan_file)
+        polygon = cols["geometry"]
+        xmin, ymin, xmax, ymax = polygon.bounds
+        
+        #First layer bounding box filter - bbox of the entire polygon
+        polygon_mask = (
+            (points[:, 0] >= xmin) & 
+            (points[:, 0] <= xmax) & 
+            (points[:, 1] >= ymin) & 
+            (points[:, 1] <= ymax)
+        )
+
+        filtered_points = points[polygon_mask]
+
+        #Buffer whose bbox will fit inside the poly
+        inner_polygon = polygon.buffer(-100)
+        ixmin, iymin, ixmax, iymax = inner_polygon.bounds
+
+        #bbox filter for the inside poly
+        inner_bbox_mask = (
+            (filtered_points[:, 0] >= ixmin) & 
+            (filtered_points[:, 0] <= ixmax) & 
+            (filtered_points[:, 1] >= iymin) & 
+            (filtered_points[:, 1] <= iymax)
+        )
+
+        #points in the inner bounding box are assumed that are completely inside
+        inner_indices = np.where(polygon_mask)[0][inner_bbox_mask]
+
+        #remaining points (in the outer but not in inner bbox)
+        border_points = filtered_points[~inner_bbox_mask]
+        border_indices = np.where(polygon_mask)[0][~inner_bbox_mask]
+
+        #split into chunks for multiprocessing
+        num_cores = 16
+        chunks = np.array_split(border_points, num_cores)
+        index_chunks = np.array_split(border_indices, num_cores)
+
+        with Pool(num_cores) as pool:
+            results = pool.map(check_points, [(chunk, polygon) for chunk in chunks])
+
+        #merge results from multiprocessing (these points are inside the border)
+        inside_polygon = np.concatenate(results)
+
+        """
+        #if the scan is completely inside the inner bbox and not in the border
+        if any(inside_polygon):
+            border_final_indices = np.concatenate(index_chunks)[inside_polygon]
+            #merge final indices
+            final_indices = np.concatenate((inner_indices, border_final_indices))
+        else:
+            new_filename = os.path.join(out_folder, str(scan_file.split("\\")[-1].split(".")[0] + "^" + cols["godlo"] + ".las"))
+            las.write(new_filename)
+        """
+
+        #if the clipping is not empty
+        if inside_polygon.any():
+            border_final_indices = np.concatenate(index_chunks)[inside_polygon]
+            #merge final indices
+            final_indices = np.concatenate((inner_indices, border_final_indices))
+
+            clipped_scan = laspy.LasData(las.header)
+            clipped_scan.points = las.points[final_indices].copy()
+
+            new_filename = os.path.join(out_folder, str(scan_file.split("\\")[-1].split(".")[0] + "^" + cols["godlo"] + ".las"))
+            clipped_scan.write(new_filename)
+
+        else: #it means that the scan is completely in the inner polygon not the border
+            new_filename = os.path.join(out_folder, str(scan_file.split("\\")[-1].split(".")[0] + "^" + cols["godlo"] + ".las"))
+            las.write(new_filename)
+
+def merge_files(godlo, file_list, output_path):
+    if file_list:
+        print(f"Merging {godlo}")
+        cmd = ['lasmerge', '-i'] + file_list + ["-o", output_path]
+        subprocess.run(cmd)
+
+def merge_clouds():
+    shape = gpd.read_file(shapefile)
+    all_files = [f for f in os.listdir(out_folder) if f.endswith(".las")]
+
+    godlo_to_files = defaultdict(list)
+    for file in all_files:
+        for godlo in shape["godlo"]:
+            if godlo in file:
+                godlo_to_files[godlo].append(os.path.join(out_folder, file))
+    
+    with ThreadPoolExecutor(max_workers = 4) as executor:
+        for _, row in shape.iterrows():
+            godlo = row["godlo"]
+            out_path = os.path.join(joined_folder, godlo + ".las")
+            executor.submit(merge_files, godlo, godlo_to_files.get(godlo, []), out_path)
+
+def main():
+    check_root()
+
+    for scan in tqdm(las_files, desc = "Clipping", total = len(las_files)):
+        process_scans(scan)
+
+    merge_clouds()
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,61 +1,133 @@
-import os
 from osgeo import gdal
+import os
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+import geopandas as gpd
+from shapely.geometry import box
+import tempfile
+from collections import defaultdict
+import shutil
 
 gdal.TermProgress = gdal.TermProgress_nocb
 
-root_folder = r"D:\___WodyPolskie\Ostrzeszow\przetwarzanie\ortho\monster\out2"
-clip_path = r"D:\___WodyPolskie\Ostrzeszow\przetwarzanie\Gotowe\clip.shp"
+root_folder = r"D:\___WodyPolskie\Ostrzeszow\przetwarzanie\Gotowe\__Przycinanie_godlo"
 
-tiff_files = []
+tif_folder = os.path.join(root_folder, "tif")
+out_folder = os.path.join(root_folder, "out_tif")
+joined_folder = os.path.join(root_folder, "joined_tif")
+shapefile = os.path.join(root_folder, "PL1992_5000_1.shp")
 
-for dirpath, dirnames, filenames in os.walk(root_folder):
-    for file in filenames:
-        if file.lower().endswith(".tif"):
-            full_path = os.path.join(dirpath, file)
-            tiff_files.append(full_path)
+tif_files = [f for f in os.listdir(tif_folder) if f.endswith(".tif")]
+folder_list = [tif_folder, out_folder, joined_folder]
 
-def raster_extent(tile):
-    ds = gdal.Open(tile)
+def check_root():
+    if os.path.isdir(root_folder):
+        for folder in folder_list:
+            name = folder.split("\\")[-1]
+            if not os.path.isdir(folder):
+                os.mkdir(folder)
+                print(f"Created folder for {name}")
+    else:
+        input("Root folder does not exist, PRESS ENTER TO EXIT")
+        raise SystemExit(0)
+    
+    if not os.path.isfile(shapefile):
+        input("Shapefile not found, PRESS ENTER TO EXIT")
+        raise SystemExit(0)
+
+    if not tif_files:
+        input("No .tif files in directory, PRESS ENTER TO EXIT")
+        raise SystemExit(0)
+
+#for tiff in tqdm(tiff_files, total = len(tiff_files)):
+def process_raster(tif):
+    shape = gpd.read_file(shapefile)
+    tif_path = os.path.join(tif_folder, tif)
+    
+    ds = gdal.Open(tif_path)
     gt = ds.GetGeoTransform()
     cols = ds.RasterXSize
     rows = ds.RasterYSize
-    minx = gt[0]
-    maxx = gt[0] + cols * gt[1]
-    miny = gt[3] + rows * gt[5]
-    maxy = gt[3]
+    bbox = box(gt[0], gt[3] + rows * gt[5], gt[0] + cols * gt[1], gt[3])
 
-    return (minx, miny, maxx, maxy)
+    intersecting_polygons = shape[shape.intersects(bbox)]
 
-def clip_polygon(tile, clip_path):
-    temp_clip = os.path.join(root_folder, "temp_clip.shp")
+    for rows, cols in intersecting_polygons.iterrows():
+        single_shape = gpd.GeoDataFrame([cols], crs = "EPSG:2180")
 
-    extent = raster_extent(tile)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            temp_vector = os.path.join(tmpdir, "cutline.geojson")
+            single_shape.to_file(temp_vector, driver = "GeoJSON")
 
-    minx, miny, maxx, maxy = extent
+            clip_name = cols["godlo"]
+            out_path = os.path.join(out_folder, tif.split(".")[0] + "^" + clip_name + ".tif")
 
-    os.system(f"ogr2ogr -clipsrc {minx} {miny} {maxx} {maxy} {temp_clip} {clip_path}")
+            overview = gdal.Open(tif_path, 1)
+            if overview.GetRasterBand(1).GetOverviewCount() == 0:
+                gdal.SetConfigOption("COMPRESS_OVERVIEW", "LZW")
+                overview.BuildOverviews("NEAREST", [2, 4, 8, 16, 32, 64], gdal.TermProgress)
+            del overview
 
-    return temp_clip
+            warp_options = gdal.WarpOptions(
+                format = "GTiff",
+                cutlineDSName = temp_vector,
+                multithread = True,
+                creationOptions = ["COMPRESS=DEFLATE", "BIGTIFF=YES"],
+                warpOptions = ["NUM_THREADS=2"],
+                dstSRS = "EPSG:2180",
+                cropToCutline = True,
+                callback = gdal.TermProgress
+            )
 
+            warp = gdal.Warp(out_path, tif_path, options = warp_options)
+            warp = None
 
-def warp_tiles(tiles, clip_path):
-    for tile in tqdm(tiles, total = len(tiles)):
-        clip = clip_polygon(tile, clip_path)
-        file = os.path.split(tile)[1]
-        folder = os.path.split(tile)[0]
+def merge_group(godlo, file_list, output_path):
+    print(f"merging {godlo}")
+    if len(file_list) == 1:
+        shutil.copyfile(file_list[0], output_path)
 
-        out_folder = os.path.join(folder, "out")
-        if not os.path.isdir(out_folder):
-            os.makedirs(out_folder, exist_ok = True)
-
-        out = os.path.join(out_folder, file)
-
-        warp_options = gdal.WarpOptions(format = "GTiff", cutlineDSName = clip, creationOptions = ["COMPRESS=LZW"], dstSRS = "EPSG:2180", callback = gdal.TermProgress)
-        warp = gdal.Warp(out, tile, options = warp_options)
+    elif len(file_list) > 1:
+        warp_options = gdal.WarpOptions(
+            format = "GTiff",
+            multithread = True,
+            creationOptions = ["COMPRESS=DEFLATE", "BIGTIFF=YES"],
+            warpOptions = ["NUM_THREADS=5"],
+            dstSRS = "EPSG:2180",
+            callback = gdal.TermProgress
+        )
+    
+        warp = gdal.Warp(output_path, file_list, options = warp_options)
         warp = None
 
-        os.remove(clip)
+def merge_tiffs():
+    shape = gpd.read_file(shapefile)
+    all_files = [f for f in os.listdir(out_folder) if f.endswith(".tif")]
+    godlo_to_files = defaultdict(list)
 
+    for file in all_files:
+        for godlo in shape["godlo"]:
+            if godlo in file:
+                godlo_to_files[godlo].append(os.path.join(out_folder, file))
 
-warp_tiles(tiff_files, clip_path)
+    with ProcessPoolExecutor(max_workers = 15) as executor:
+        futures = []
+        
+        for _, row in shape.iterrows():
+            godlo = row["godlo"]
+            out_path = os.path.join(joined_folder, f"{godlo}.tif")
+            futures.append(executor.submit(merge_group, godlo, godlo_to_files.get(godlo, []), out_path))
+
+        for f in tqdm(futures, desc = "Merging"):
+            f.result()
+
+def main():
+    check_root()
+
+    #with ProcessPoolExecutor(max_workers = 15) as executor:
+        #list(tqdm(executor.map(process_raster, tif_files), total = len(tif_files), desc = "Clipping"))
+    
+    merge_tiffs()
+
+if __name__ == "__main__":
+    main()
